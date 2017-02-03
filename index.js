62cd87012b7e669ab180ebbfb99d5764f6922a7f
Antoine Motet <antoine.motet@epitech.eu>
2017-02-03 16:21:32 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+
+const rules = {
+    indent: [
+        'error', 4,
+        {
+            SwitchCase: 1,
+        },
+    ],
+
+    'linebreak-style': [
+        'error', 'unix',
+    ],
+
+    quotes: [
+        'error', 'single',
+        {
+            avoidEscape: true,
+            allowTemplateLiterals: false,
+        },
+    ],
+
+    semi: [
+        'error', 'never',
+    ],
+}
+
+
+
+module.exports = {
+    'extends': 'eslint:recommended',
+
+    globals: {
+        global: true,
+        require: true,
+        module: true,
+        process: true,
+        console: true,
+        Promise: true,
+
+        setInterval: true,
+        clearInterval: true,
+        setTimeout: true,
+        clearTimeout: true,
+
+        // Mocha
+        it: true,
+        describe: true,
+    },
+
+    parser: 'babel-eslint',
+
+    parserOptions: {
+        ecmaVersion: 8,
+
+        ecmaFeatures: {
+            impliedStrict: true,
+            experimentalObjectRestSpread: true,
+            jsx: true,
+        },
+
+        sourceType: 'module',
+    },
+
+    rules,
+}
